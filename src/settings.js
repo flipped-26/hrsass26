b6db51085519e8afd9d1fca7a451665c778e0b7e
flipped-26 <15870812441@163.com>
2022-06-19 14:19:30 +0800

    人力资源初始化

---
+++ src/settings.js
@@ -0,0 +1,20 @@
+// 项目配置文件
+module.exports = {
+
+  // 项目标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 头部固定
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏logo是否显示
+  sidebarLogo: false
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,23 @@
+// 用户信息模块仓库数据
+
+const state = {
+
+};
+const mutations = {
+  
+};
+const actions = {
+
+};
+const getters = {
+
+};
+export default {
+  //子模块高封闭性，开启命名空间
+  namespaced:true,
+  state,
+  mutations,
+  actions,
+  getters
+
+}
